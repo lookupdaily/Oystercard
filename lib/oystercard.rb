9af4376cfd34b66023a94dcef2ec036cab82e569
Student <student@Makerss-MacBook-Air-2.local>
2020-01-28 15:06:45 +0000

    Step 8 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,30 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :status
   Limit = 90
+  Minimum_fare = 1
   def initialize
-    @balance = 0
+    @balance = 10
+    @status = false
   end
 
   def top_up(value)
     fail 'Top up limit exceeded' if value + balance > Limit
     @balance += value
+  end
+
+  def deduct(fair)
+    @balance = @balance - fair
+  end
+
+  def in_journey?
+    @status
+  end
+
+  def touch_in
+    @status = true
+  end
+
+  def touch_out
+    @status = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,20 +1,47 @@
 require 'oystercard'
 describe Oystercard do
   it "new card should have a balance of zero" do
-    expect(subject.balance).to eq 0
+    expect(subject.balance).to eq 10
   end
 
   it { is_expected.to respond_to(:balance) }
 
-  it { expect(subject).to respond_to(:top_up).with(1).argument }
+  describe '#top_up' do
+    it { expect(subject).to respond_to(:top_up).with(1).argument }
 
-  it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'raises an error when the limit is exceeded' do
+      maximum_balance = Oystercard::Limit
+      subject.top_up(80)
+      expect{subject.top_up 1 }.to raise_error 'Top up limit exceeded'
+    end
   end
 
-  it 'raises an error when the limit is exceeded' do
-    maximum_balance = Oystercard::Limit
-    subject.top_up(maximum_balance)
-    expect{subject.top_up 1 }.to raise_error 'Top up limit exceeded'
+  describe '#deduct' do
+    it 'should deduct the fair amount from the balance' do
+      expect{subject.deduct Oystercard::Minimum_fare }.to change{ subject.balance }.by -(Oystercard::Minimum_fare)
+    end
+  end
+
+  describe '#in_journey?' do
+    it 'should return true or false' do
+      expect(subject.in_journey?).to eq false
+    end
+  end
+
+  describe '#touch_in' do
+    it 'should change status to true' do
+      expect{ subject.touch_in }.to change{ subject.status }.to true
+    end
+  end
+
+  describe '#touch_out' do
+    it 'should change status to false' do
+      subject.touch_out
+      expect(subject.status).to eq false
+    end
   end
 end
